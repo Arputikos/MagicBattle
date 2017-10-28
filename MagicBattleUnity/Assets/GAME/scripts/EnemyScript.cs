ab12d075be8741d3456552e94f2bb83a8477b9f5
arputikos <arputikos11@op.pl>
2017-10-28 15:53:14 +0200

    VR and zombies

---
+++ MagicBattleUnity/Assets/GAME/scripts/EnemyScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    GameObject playerHead;
+    public float speed;
+
+    public float minDist = 0.1f;
+    public float DEAD_VISIBLE = 3.0f;
+
+    bool lives = true;
+
+    float dieTimer = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerHead = GameObject.FindGameObjectWithTag("Head");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!lives)
+        {
+            dieTimer += Time.deltaTime;
+            if(dieTimer > DEAD_VISIBLE)
+            {
+                Destroy(this.gameObject);
+            }
+            return;
+        }
+
+        Vector3 diff = playerHead.transform.position - this.transform.position;
+        diff.y = this.transform.position.y;
+
+        if (diff.magnitude >= minDist)
+        {
+            
+            this.transform.position += diff.normalized * speed * Time.deltaTime;
+            Vector3 face = playerHead.transform.position;
+            face.y = this.transform.position.y;
+            this.transform.LookAt(face);
+
+            //if(this.GetComponentInChildren<Animator>().GetCurrentAnimatorStateInfo(0).IsName("attack"))
+            if(this.GetComponentInChildren<Animator>().GetInteger("ggg") == 1)
+            {
+                this.GetComponentInChildren<Animator>().SetInteger("ggg", 0);
+                //this.GetComponentInChildren<Animator>().Play("walk", 0, 0.0f);
+            }
+        }
+        else
+        {
+            this.GetComponentInChildren<Animator>().SetInteger("ggg", 1);
+            //at player
+            //this.GetComponentInChildren<Animator>().Play("attack", 0, 0.0f);
+        }
+
+
+
+    }
+
+    private void Die()
+    {
+        if (!lives)
+            return;
+
+        lives = false;
+        dieTimer = 0;
+        this.GetComponentInChildren<Animator>().SetInteger("ggg", 2);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("fireball"))
+        {
+            other.gameObject.GetComponent<FireballScript>().Collided();
+            Die();
+        }
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Die();
+    //}
+}
---
+++ MagicBattleUnity/Assets/GAME/scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleInput();
+
+    }
+
+    void HandleInput()
+    {
+        //A = button0
+        //B = 1
+        //X = 2
+        //Y = 3
+        //debug
+        //foreach (KeyCode kcode in System.Enum.GetValues(typeof(KeyCode)))
+        //{
+        //    if (Input.GetKeyDown(kcode))
+        //        Debug.Log("KeyCode down: " + kcode);
+        //}
+
+        if(Input.GetKeyDown(KeyCode.JoystickButton0))
+        {
+            player.GetComponent<PlayerController>().Shoot();
+        }
+    }
+}
