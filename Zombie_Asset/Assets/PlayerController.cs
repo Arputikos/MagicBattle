8b4eda51aab5b336b67725f880deef59eeb6d9bd
Kamil41 <kamil41@orange.pl>
2017-10-28 14:18:07 +0200

    Zombie chodzą, spawnują się do określonej ilosci. Wokół gracza jest bezpeiczny obszar

---
+++ Zombie_Asset/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizntal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizntal, 0, moveVertical);
+
+        rb.AddForce(movement * speed, ForceMode.Acceleration);
+    }
+}
